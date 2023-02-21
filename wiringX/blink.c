6c5f9348fc2bc92fde7b62d236cd9005251d0c66
nascs <nascs@radxa.com>
2023-02-21 18:39:22 +0800

    fix wiringX/blink.c

--- wiringX/blink.c
+++ wiringX/blink.c
@@ -1,11 +1,3 @@
-/*
-	Copyright (c) 2016 CurlyMo <curlymoo1@gmail.com>
-
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,7 +16,6 @@ int main(int argc, char *argv[]) {
 	pinMode(gpio, PINMODE_OUTPUT);
 
 	while(1) {
-		printf("Writing to GPIO %d: HIGH\n", gpio, digitalRead(gpio));
 		digitalWrite(gpio, HIGH);
 		sleep(1);
 		digitalWrite(gpio, LOW);
