4bb70ae1eb5c2fab6739fc879305bca541a8cff9
nascs <105425502+nascs@users.noreply.github.com>
2023-03-11 20:54:55 +0800

    fix code style in the file read.py

--- wiringX/python/read.py
+++ wiringX/python/read.py
@@ -18,9 +18,9 @@ gpio.pinMode(gpio.PIN8, gpio.PINMODE_INPUT);
 
 try:
 	while True:
-		print "Reading from pin 8: "+str(gpio.digitalRead(gpio.PIN8));
+		print("Reading from pin 8: "+str(gpio.digitalRead(gpio.PIN8)));
 		sleep(1);
-		print "Reading from pin 8: "+str(gpio.digitalRead(gpio.PIN8));
+		print("Reading from pin 8: "+str(gpio.digitalRead(gpio.PIN8)));
 		sleep(1);
 except KeyboardInterrupt:
 	pass
