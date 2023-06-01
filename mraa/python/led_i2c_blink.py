f9971b58d9200cfac1fb19f819216d14b71d3648
nascs <nascs@radxa.com>
2023-06-01 16:30:11 +0800

    add i2c led test for mraa python version

---
+++ mraa/python/led_i2c_blink.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import mraa
+import time
+
+# initialise I2C
+x = mraa.I2c(2)
+x.address(0x20)
+while True:
+    x.writeReg(0x11, 0x01)
+    time.sleep(0.5)
+    x.writeReg(0x12, 0x02)
+    time.sleep(0.5)
+    x.writeReg(0x13, 0x03)
+    time.sleep(0.5)
+    x.writeReg(0x14, 0x04)
+    time.sleep(0.5)
+    x.writeReg(0x15, 0x05)
+    time.sleep(0.5)
+    x.writeReg(0x16, 0x06)
