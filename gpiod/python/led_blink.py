303e2a5ffd07325307bbb325f76160cd3cbe7d29
nascs <nascs@radxa.com>
2023-09-06 03:59:14 +0000

    gpiod: py: add led_blink

---
+++ gpiod/python/led_blink.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import gpiod
+import time
+
+chip_name = "gpiochip3"
+line_offset = 4
+
+chip = gpiod.chip(chip_name)
+line = chip.get_line(line_offset)
+
+config = gpiod.line_request()
+config.consumer = "GPIO3_A4_Blink"
+config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+
+line.request(config)
+
+print(line.consumer)
+
+while True:
+    line.set_value(0)
+    time.sleep(0.1)
+    line.set_value(1)
+    time.sleep(0.1)
