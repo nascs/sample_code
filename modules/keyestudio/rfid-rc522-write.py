4d9c204e178cac5b0fc44a1afb6c2c12757dee2e
Nascs <nascs@radxa.com>
2023-09-21 09:39:31 +0000

    update code style of rfid-rc522-write.py

--- modules/keyestudio/rfid-rc522-write.py
+++ modules/keyestudio/rfid-rc522-write.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
 import OPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import time
 
 rfid_rc522 = SimpleMFRC522()
-data = "0x110101001"
+data = "Hello, world"
 
 rfid_rc522.write(data)
 
