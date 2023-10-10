cf6992a1fb862c2b6dfbff231df2bf349b837422
nascs <nascs@radxa.com>
2023-10-10 10:52:25 +0000

    mraa: add env

---
+++ mraa/env.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo apt-get install -y build-essential swig python-dev python3-dev libnode-dev  cmake libjson-c-dev
+
+sudo apt-get install -y libc6 libgcc1 libstdc++6 python python2.7 libpython2.7 python3.9 libpython3.9 python3-dev python3
+
+sudo apt-get install -y libgtest-dev pkg-config cmake-data
