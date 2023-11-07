ea9985a76ab828607ff66ccf8fc11160984c10ec
Nascs <nascs@radxa.com>
2023-11-07 11:31:46 +0000

    mraa: py: add gpio_read

---
+++ mraa/python/gpio_read.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import mraa
+import time
+
+pin = 3  # set pin num
+
+try:
+    gpio = mraa.Gpio(pin)
+    gpio.dir(mraa.DIR_IN)  # set mode as input
+
+    while True:
+        value = gpio.read()  # read from pin 3
+        print("value: {}".format(value))
+        time.sleep(1)
+        
+except KeyboardInterrupt:
+    print("\nstop\n")
